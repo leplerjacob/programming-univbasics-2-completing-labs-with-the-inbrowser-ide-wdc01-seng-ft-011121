4feff37d56dc7d0f690b7c87b20eeaafcab97554
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-23 20:51:25 +0000

    Done.

--- spec/first_lab_spec.rb
+++ spec/first_lab_spec.rb
@@ -6,3 +6,9 @@ describe 'lib/your_solution.rb' do
     require_relative '../lib/your_solution.rb'  # load/run the file 
   end
 end
+describe 'lib/your_second_solution.rb' do
+  it 'should output "Hello World Again!" when run' do
+    expect(STDOUT).to receive(:puts).with('Hello World Again!')
+    require_relative '../lib/your_second_solution.rb' # load/run the file
+  end
+end
